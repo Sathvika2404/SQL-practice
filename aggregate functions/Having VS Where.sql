d6d4f62ce671b52a728e65f816e6706fd62eec8c
BOLISHETTY SATHVIKA <116671555+Sathvika2404@users.noreply.github.com>
2026-01-20 21:12:06 -0600

    Add files via upload

---
+++ aggregate functions/Having VS Where.sql
@@ -0,0 +1,60 @@
+﻿-- Having VS Where
+
+select * from sales 
+where productID=1 -- where clause is used to filter out the data from the database
+
+select * from sales 
+
+
+
+
+select productID ,
+sum(totalAMount) as SumSales 
+from sales 
+group by productID having sum(totalAMount)>300
+
+/* WHERE filters rows.
+HAVING filters groups.
+
+SQL execution order:
+FROM
+→ WHERE
+→ GROUP BY
+→ HAVING
+→ SELECT
+→ ORDER BY
+
+
+WHERE
+
+Runs before aggregation
+
+Works on individual rows
+
+Cannot use aggregate functions (SUM, AVG, COUNT, etc.)
+
+Filters rows before any grouping.
+
+
+
+
+HAVING
+
+Runs after aggregation
+
+Works on groups
+
+Can use aggregate functions
+
+
+
+Rule you should internalize
+
+If the condition involves aggregates → HAVING
+If it involves raw column values → WHERE
+
+No exceptions.
+*/
+
+
+
