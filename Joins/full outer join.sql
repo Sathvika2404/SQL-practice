334d55ddd7fe256f0f798b132ef69b960af4868e
BOLISHETTY SATHVIKA <116671555+Sathvika2404@users.noreply.github.com>
2026-01-20 21:13:02 -0600

    Add files via upload

---
+++ Joins/full outer join.sql
@@ -0,0 +1,8 @@
+--full outer join
+
+select * from table1
+select * from table2
+
+select * from table1
+full outer join table2
+on table1.c1=table2.c1
---
+++ Joins/right join.sql
@@ -0,0 +1,16 @@
+-- right join
+
+select * from table1
+select * from table2
+
+select * from table1
+right join table2
+on table1.c1=table2.c1
+
+
+
+
+
+
+
+
