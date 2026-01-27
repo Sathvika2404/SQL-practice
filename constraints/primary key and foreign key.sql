bf6cb5840f4ea43ebbd43e6c91f77f88f8e60d61
BOLISHETTY SATHVIKA <116671555+Sathvika2404@users.noreply.github.com>
2026-01-27 13:06:59 -0600

    Add files via upload

---
+++ constraints/primary key and foreign key.sql
@@ -0,0 +1,17 @@
+-- primary key and foreign key 
+
+/*
+PRIMARY KEY
+
+A Primary Key uniquely identifies each row in a table.
+
+Rules (non-negotiable)
+ 1. Must be UNIQUE
+ 2. Must be NOT NULL
+ 3. Only ONE PK per table
+
+Can be:
+Single column
+Composite (multiple columns)
+*/
+
